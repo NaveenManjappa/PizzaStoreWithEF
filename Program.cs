d978aeeded9bfee78bfbd750437f89852f4c7e74
Naveen Manjappa <NManjappa@manh.com>
2024-10-04 16:31:08 +0100

    Adding the api routes with db operations

--- Program.cs
+++ Program.cs
@@ -30,4 +30,33 @@ app.MapGet("/", () => "Hello World!");
 //Pizza store apis
 app.MapGet("/pizzas",async (PizzaDb db)=> await db.Pizzas.ToListAsync());
 
+app.MapPost("/pizzas",async (PizzaDb db,Pizza pizza)=>{
+  await db.Pizzas.AddAsync(pizza);
+  await db.SaveChangesAsync();
+  return Results.Created($"/pizzas/{pizza.Id}",pizza);
+});
+
+app.MapGet("/pizzas/{id}",async(PizzaDb db,int id) => await db.Pizzas.FindAsync(id));
+
+app.MapPut("/pizzas/{id}",async (PizzaDb db,Pizza updatePizza,int id)=>{
+  var pizza=await db.Pizzas.FindAsync(id);
+  if(pizza is null) return Results.NotFound();
+
+  pizza.Name = updatePizza.Name;
+  pizza.Description = updatePizza.Description;
+
+  await db.SaveChangesAsync();
+  return Results.NoContent();
+});
+
+app.MapDelete("/pizzas/{id}",async (PizzaDb db, int id) => {
+  var pizza=await db.Pizzas.FindAsync(id);
+
+  if(pizza is null) return Results.NotFound();
+
+  db.Pizzas.Remove(pizza);
+  await db.SaveChangesAsync();
+  return Results.Ok();
+});
+
 app.Run();
