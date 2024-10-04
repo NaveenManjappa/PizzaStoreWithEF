7ca2ffd62d9af4646ea815476867876617609efb
Naveen Manjappa <NManjappa@manh.com>
2024-10-04 14:59:13 +0100

    Adding swagger to the api project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
+
+
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c=>{
+  c.SwaggerDoc("v1",new OpenApiInfo
+        {Title="PizzaStore API",Description="Making the Pizzas you love",Version="v1"}
+        );
+});
+
 var app = builder.Build();
+
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>{
+  c.SwaggerEndpoint("/swagger/v1/swagger.json","Pizza Store API V100");
+});
 
 app.MapGet("/", () => "Hello World!");
 
